ccca588184fddcdd1686ac476d49d77248aa6ab0
Thomas Alrek <thomas@alrek.no>
2020-01-22 23:11:08 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Netflex Foundation library
---
+++ src/Setting.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Netflex\Foundation;
+
+use Netflex\API\Facades\API;
+
+use Netflex\Support\Retrievable;
+use Netflex\Support\ReactiveObject;
+
+use Illuminate\Support\Facades\Cache;
+
+class Setting extends ReactiveObject
+{
+  use Retrievable;
+
+  protected static $base_path = 'foundation/variables';
+
+  /** @var array */
+  protected $timestamps = [];
+
+  /**
+   * Retrieve the value of a Setting
+   *
+   * @param string $key
+   * @return mixed
+   */
+  public static function get($key)
+  {
+    $setting = static::retrieve($key);
+    return $setting ? $setting->value : null;
+  }
+
+  /**
+   * @param mixed $value
+   * @return mixed
+   */
+  public function getValueAttribute($value)
+  {
+    switch ($this->format) {
+      case 'boolean':
+        return (bool) (int) $value;
+      case 'json':
+        if (is_string($value)) {
+          return json_decode($value);
+        }
+
+        return $value;
+      default:
+        return $value;
+    }
+  }
+
+  /**
+   * @return static[]
+   */
+  public static function all()
+  {
+    $templates = Cache::rememberForever('variables', function () {
+      return API::get('foundation/variables');
+    });
+
+    return collect($templates)->map(function ($content) {
+      return new static($content);
+    });
+  }
+
+  /**
+   * @param string $alias
+   * @return static|void
+   */
+  public static function retrieve($alias)
+  {
+    return static::all()->first(function ($content) use ($alias) {
+      return $content->alias === $alias;
+    });
+  }
+}
